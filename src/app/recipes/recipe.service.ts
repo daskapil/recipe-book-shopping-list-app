92902405bcaa11c9abcb7d02e79553c6dcd3922f
Kapil Das <kapil.das.etc@gmail.com>
2021-01-15 22:02:53 -0500

    added the httpclient

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -8,34 +8,40 @@ import { Recipe } from "./recipe.model";
     providedIn:'root'
 })
 export class RecipeService { 
+    private recipes: Recipe[] = [];
     recipesChanged = new Subject<Recipe[]>();
 
-    private recipes: Recipe[] = [
-        new Recipe(
-            'Shahi Chicken Curry',
-            'Shahi Checiken Curry ',
-            'https://picturetherecipe.com/wp-content/uploads/2020/01/Rogan-Josh-by-PictureTheRecipe-Featured-1-395x500.jpg',
-            [
-                new Ingredient('Chicken', 500),
-                new Ingredient('Onion', 200),
-                new Ingredient('Tomato', 100)
-            ]
-        ),
+    // private recipes: Recipe[] = [
+    //     new Recipe(
+    //         'Shahi Chicken Curry',
+    //         'Shahi Checiken Curry ',
+    //         'https://picturetherecipe.com/wp-content/uploads/2020/01/Rogan-Josh-by-PictureTheRecipe-Featured-1-395x500.jpg',
+    //         [
+    //             new Ingredient('Chicken', 500),
+    //             new Ingredient('Onion', 200),
+    //             new Ingredient('Tomato', 100)
+    //         ]
+    //     ),
         
-        new Recipe(
-            'Aloo Posto',
-            'Delicious Aloo Posto',
-            'https://uploads-ssl.webflow.com/5c481361c604e53624138c2f/5c78acdb86beb7344fdb888f_Alu-posto-website-thumbnail.png.jpg',
-            [
-                new Ingredient('Popi Seed', 500),
-                new Ingredient('Potato', 200),
-                new Ingredient('Onion', 50)
-            ]
-        )
-    ];
+    //     new Recipe(
+    //         'Aloo Posto',
+    //         'Delicious Aloo Posto',
+    //         'https://uploads-ssl.webflow.com/5c481361c604e53624138c2f/5c78acdb86beb7344fdb888f_Alu-posto-website-thumbnail.png.jpg',
+    //         [
+    //             new Ingredient('Popi Seed', 500),
+    //             new Ingredient('Potato', 200),
+    //             new Ingredient('Onion', 50)
+    //         ]
+    //     )
+    // ];
 
     constructor(private shoppingListService: ShoppingListService) { }
-        
+    
+    setRecipes(recipes: Recipe[]) {
+        this.recipes = recipes;
+        this.recipesChanged.next(this.recipes.slice());
+    }
+
     getRecipes() {
         return this.recipes.slice();
     }
@@ -62,6 +68,5 @@ export class RecipeService {
         this.recipes.splice(index, 1);
         this.recipesChanged.next(this.recipes.slice());
     }
-
 
 }
